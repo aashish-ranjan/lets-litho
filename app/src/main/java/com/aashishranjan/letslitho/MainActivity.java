5d135c3bddf3c69675f0d49dfd58363946162606
aashish-swiggy <aashish.ranjan@swiggy.in>
2021-03-01 22:11:45 +0530

    add project files

---
+++ app/src/main/java/com/aashishranjan/letslitho/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aashishranjan.letslitho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
