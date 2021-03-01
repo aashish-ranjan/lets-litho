1bbf235b38a95d0030ac0a056c5ff9c9ddbc74c5
aashish-swiggy <aashish.ranjan@swiggy.in>
2021-03-01 22:33:39 +0530

    print Hello Litho

--- app/src/main/java/com/aashishranjan/letslitho/MainActivity.java
+++ app/src/main/java/com/aashishranjan/letslitho/MainActivity.java
@@ -4,10 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.LithoView;
+import com.facebook.litho.widget.Text;
+import com.facebook.soloader.SoLoader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SoLoader.init(this, false);
+
+        final ComponentContext c = new ComponentContext(this);
+
+        final Component textComponent = Text.create(c)
+            .text("Hello Litho")
+            .textSizeDip(50)
+            .build();
+
+        setContentView(LithoView.create(c, textComponent));
     }
 }
