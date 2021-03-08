b630e77f809f212cbe6e6ca17fc6d4c2de328a85
aashish-swiggy <aashish.ranjan@swiggy.in>
2021-03-09 03:01:02 +0530

    display lottie file using mountSpec

--- app/src/main/java/com/aashishranjan/letslitho/CustomLottieAnimationViewSpec.java
+++ app/src/main/java/com/aashishranjan/letslitho/CustomLottieAnimationViewSpec.java
@@ -1,46 +1,25 @@
 package com.aashishranjan.letslitho;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
-import com.facebook.litho.Output;
 import com.facebook.litho.Size;
 import com.facebook.litho.SizeSpec;
-import com.facebook.litho.annotations.FromPrepare;
 import com.facebook.litho.annotations.MountSpec;
 import com.facebook.litho.annotations.OnCreateMountContent;
 import com.facebook.litho.annotations.OnMeasure;
 import com.facebook.litho.annotations.OnMount;
-import com.facebook.litho.annotations.OnPrepare;
 import com.facebook.litho.annotations.Prop;
 
 @MountSpec
-public class ColorComponentSpec {
+public class CustomLottieAnimationViewSpec {
 
     @OnCreateMountContent
-    static ColorDrawable onCreateMountContent(Context context) {
-        return new ColorDrawable();
-    }
-
-    //a heavy operation, can be run on BG thread
-    @OnPrepare
-    static void onPrepare(
-        ComponentContext c,
-        @Prop String colorName,
-        Output<Integer> color) {
-        color.set(Color.parseColor(colorName));
-    }
-
-    @OnMount
-    static void onMount(
-        ComponentContext c,
-        ColorDrawable colorDrawable,
-        @FromPrepare int color) {
-
-        colorDrawable.setColor(color);
+    static LottieAnimationView onCreateMountContent(Context c) {
+        return new LottieAnimationView(c);
     }
 
     @OnMeasure
@@ -51,18 +30,30 @@ public class ColorComponentSpec {
         int heightSpec,
         Size size) {
 
-        //set default width(in px) when no width is specified
+        //set default width when no width is specified
         if (SizeSpec.getMode(widthSpec) == SizeSpec.UNSPECIFIED) {
-            size.width = 40;
+            size.width = 100;
         } else {
             size.width = SizeSpec.getSize(widthSpec);
         }
 
-        //set default height(in px) when no height is specified
+        //set default height when no height is specified
         if (SizeSpec.getMode(heightSpec) == SizeSpec.UNSPECIFIED) {
-            size.height = (int) (size.width * 1.5);
+            size.height = 100;
         } else {
             size.height = SizeSpec.getSize(heightSpec);
         }
     }
+
+    @OnMount
+    static void onMount(
+        ComponentContext c,
+        LottieAnimationView animationView,
+        @Prop int lottieResource) {
+
+        animationView.setAnimation(lottieResource);
+        animationView.setRepeatCount(LottieDrawable.INFINITE);
+        animationView.playAnimation();
+    }
+
 }
--- app/src/main/java/com/aashishranjan/letslitho/MainActivity.java
+++ app/src/main/java/com/aashishranjan/letslitho/MainActivity.java
@@ -1,10 +1,16 @@
 package com.aashishranjan.letslitho;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.LithoView;
@@ -51,12 +57,32 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(LithoView.create(c, recyclerCollectionComponent));
 
         //using mount spec to create colorDrawable
-        final ColorComponent colorComponent = ColorComponent.create(c)
-            .colorName("#3F51B5")
+//        final ColorComponent colorComponent = ColorComponent.create(c)
+//            .colorName("#3F51B5")
+//            .build();
+//
+//        setContentView(LithoView.create(c, colorComponent));
+
+//        //Create lottie layout programmatically
+//        ConstraintLayout lottieLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_main, null);
+//        setContentView(lottieLayout);
+//
+//        LottieAnimationView lav = new LottieAnimationView(this);
+//        lav.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+//        lav.setAnimation(R.raw.lottie_segmented_top_up);
+//
+//        lav.setRepeatCount(LottieDrawable.INFINITE);
+//        lav.playAnimation();
+//        lav.setBackgroundColor(Color.LTGRAY);
+//
+//        lottieLayout.addView(lav);
+
+        final CustomLottieAnimationView clavComponent = CustomLottieAnimationView.create(c)
+            .lottieResource(R.raw.lottie_segmented_top_up)
+            .widthDip(100)
+            .heightDip(100)
             .build();
 
-        setContentView(LithoView.create(c, colorComponent));
-
-
+        setContentView(LithoView.create(c, clavComponent));
     }
 }
